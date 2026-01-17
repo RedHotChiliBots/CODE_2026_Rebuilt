7ee6f405250ea589b9f48693a8a1da600ccbffc0
sdseifert <sseifert@redhotchilibots.com>
2026-01-17 17:22:54 -0500

    changes for #9

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -7,9 +7,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class Hopper extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public Hopper() {
+    
+  }
 
   /**
    * Example command factory method.
