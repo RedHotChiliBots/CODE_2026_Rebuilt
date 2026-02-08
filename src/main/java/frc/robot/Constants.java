f5a989cc18be84549205787589f44fd9d43dab74
Steve Seifert <sseifert@redhotchilibots.com>
2026-02-07 19:23:04 -0500

    31 create initial climber subsystem (#38)
    * Started Defining Motors, Servos, etc for the Climber. All numbers will need to change
    
    * Climber constructor 90% complete as far as I know. For now, all constants are shared between each motor. Numbers such as PID and offsets will need to be adjusted as the robot is built. -Mason
    
    * More climber configuration, along with methods. Servos are unoperational as of now, but simulation runs.
    
    ---------
    
    Co-authored-by: starmap789 <starmap789@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,10 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
-//import com.revrobotics.spark.config.SparkBaseConfig.IdleMode; 
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode; 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
@@ -34,7 +31,8 @@ public final class Constants {
 	}
 
 	public static final class PWMId {
-		public static final int kGrenadePin = 0;
+		public static final int kClimberHook1PWMId = 0;
+		public static final int kClimberHook2PWMId = 1;
 	}
 
 	public static final class CANId {
@@ -57,11 +55,12 @@ public final class Constants {
 
 		public static final int kFeederCanId = 30;
 
-		public static final int kClimberLeftCanId = 40;
-		public static final int kClimberRightCanId = 41;
+		public static final int kClimber1CanId = 40;
+		public static final int kClimber2CanId = 41;
+		public static final int kClimber3CanId = 42;
+		public static final int kClimber4CanId = 43;
 
-		public static final int kLadderLeftCanId = 50;
-		public static final int kLadderRightCanId = 51;
+		public static final int kClimberServoHubCanId = 50;
 	}
 
 	public static final class MotorConstants {
@@ -184,6 +183,35 @@ public final class Constants {
 		public static final double Ultra3 = 2.89;
 		public static final double Ultra4 = 3.61;
 		public static final double Ultra5 = 5.23;
+	}
+
+	public static final class Climber {
+
+		//Motor Inversion
+		public static final boolean kClimberInverted = false;
+
+		//Idle Mode
+		public static final IdleMode kClimberIdleMode = IdleMode.kBrake;
+
+		//Current Limit
+		public static final int kClimberCurrentLimit = 50; // amps
+
+		//Abs Encoder Configs
+		public static final double kZeroOffset = 0.5; //WILL NEED ADJUSTMENT
+		public static final boolean kZeroCentered = true;
+		public static final boolean kEncoderInverted = true;
+		public static final double kTiltPositionFactor = 360; // 1.0 / (GearBox.Max9 * GearBox.Max5 * GearBox.Max5) * 360.0 degrees
+		public static final double kTiltVelocityFactor = kTiltPositionFactor / 60.0; // degrees per second 
+
+		//Closed loop configs
+		public static final double kPosP = 0.0; // maxmotion 0.0
+		public static final double kPosI = 0.0; // maxmotion 0.0
+		public static final double kPosD = 0.0; // maxmotion 0.0
+
+		public static final double kPosMinOutput = -0.5; //max motion -1.0
+		public static final double kPosMaxOutput = 0.5; //max motion 1.0
+
+		public static final double kTollerance = 0.5; // degrees
 	}
 
 	public static final class Vision {
