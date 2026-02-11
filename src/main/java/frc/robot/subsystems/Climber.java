01bf24bc4bc6159ad5d9af7316ca3d4244af8acf
sdseifert <sseifert@redhotchilibots.com>
2026-02-11 18:31:05 -0500

    Added methods and commands

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,176 +17,215 @@ import com.revrobotics.servohub.ServoHub;
 import com.revrobotics.servohub.config.ServoChannelConfig;
 import com.revrobotics.servohub.config.ServoHubConfig;
 
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.PWMId;
-import frc.robot.utils.Library;
 
 //CLASS DEFINITION
 public class Climber extends SubsystemBase {
 
-  //Define Motors, Servos, Etc
+  // Define Motors, Servos, Etc
   private final SparkMax climber1 = new SparkMax(
-    Constants.CANId.kClimber1CanId, MotorType.kBrushless);
+      Constants.CANId.kClimber1CanId, MotorType.kBrushless);
 
   private final SparkMax climber2 = new SparkMax(
-    Constants.CANId.kClimber2CanId, MotorType.kBrushless);
+      Constants.CANId.kClimber2CanId, MotorType.kBrushless);
 
   private final SparkMax climber3 = new SparkMax(
-    Constants.CANId.kClimber3CanId, MotorType.kBrushless);
+      Constants.CANId.kClimber3CanId, MotorType.kBrushless);
 
   private final SparkMax climber4 = new SparkMax(
-    Constants.CANId.kClimber4CanId, MotorType.kBrushless);
+      Constants.CANId.kClimber4CanId, MotorType.kBrushless);
 
   private final SparkMaxConfig climber1Config = new SparkMaxConfig();
   private final SparkMaxConfig climber2Config = new SparkMaxConfig();
   private final SparkMaxConfig climber3Config = new SparkMaxConfig();
   private final SparkMaxConfig climber4Config = new SparkMaxConfig();
-  
+
   private final SparkClosedLoopController climber1Controller = climber1.getClosedLoopController();
-  private final SparkClosedLoopController climber2Controller = climber2.getClosedLoopController();
-  private final SparkClosedLoopController climber3Controller = climber3.getClosedLoopController();
-  private final SparkClosedLoopController climber4Controller = climber4.getClosedLoopController();
+  // private final SparkClosedLoopController climber2Controller =
+  // climber2.getClosedLoopController();
+  // private final SparkClosedLoopController climber3Controller =
+  // climber3.getClosedLoopController();
+  // private final SparkClosedLoopController climber4Controller =
+  // climber4.getClosedLoopController();
 
   private final AbsoluteEncoder climber1AbsEncoder = climber1.getAbsoluteEncoder();
-  private final AbsoluteEncoder climber2AbsEncoder = climber2.getAbsoluteEncoder();
-  private final AbsoluteEncoder climber3AbsEncoder = climber3.getAbsoluteEncoder();
-  private final AbsoluteEncoder climber4AbsEncoder = climber4.getAbsoluteEncoder();
+  // private final AbsoluteEncoder climber2AbsEncoder =
+  // climber2.getAbsoluteEncoder();
+  // private final AbsoluteEncoder climber3AbsEncoder =
+  // climber3.getAbsoluteEncoder();
+  // private final AbsoluteEncoder climber4AbsEncoder =
+  // climber4.getAbsoluteEncoder();
 
-  // private final Servo hook1 = new Servo(PWMId.kClimberHook1PWMId);
-  // private final Servo hook2 = new Servo(PWMId.kClimberHook2PWMId);
-  
-  // private final ServoHub servoHub = new ServoHub(Constants.CANId.kClimberServoHubCanId);
-  // private final ServoHubConfig servoHubConfig = new ServoHubConfig();
+  private final ServoHub servoHub = new ServoHub(Constants.CANId.kServoHubCanId);
+  private final ServoChannel leftHook = servoHub.getServoChannel(ChannelId.kChannelId0);
+  private final ServoChannel rightHook = servoHub.getServoChannel(ChannelId.kChannelId1);
 
-  // ServoChannel channel0 = servoHub.getServoChannel(ChannelId.kChannelId0);
-  // ServoChannel channel1 = servoHub.getServoChannel(ChannelId.kChannelId1);
+  private final ServoHubConfig hubConfig = new ServoHubConfig();
 
-  
-  public enum ClimberSP { //Climber Setpoints
-    ZERO(0), //NUMBERS NEED TO CHANGE
-    STOW(1), //NUMBERS NEED TO CHANGE
-    READY(2), //NUMBERS NEED TO CHANGE
-    CLIMB(3); //NUMBERS NEED TO CHANGE
+  public enum ClimberSP { // Climber Setpoints
+    STOW(0.5), // NUMBERS NEED TO CHANGE
+    TOP(1.0), // NUMBERS NEED TO CHANGE
+    BOT(0.0), // NUMBERS NEED TO CHANGE
+    LVLAUTON(0.25), // NUMBERS NEED TO CHANGE
+    LVL1(1.0), // NUMBERS NEED TO CHANGE
+    LVL2(0.0), // NUMBERS NEED TO CHANGE
+    LVL3(1.0); // NUMBERS NEED TO CHANGE
 
     private final double sp;
 
-		ClimberSP(final double sp) {
-			this.sp = sp;
-		}
+    ClimberSP(final double sp) {
+      this.sp = sp;
+    }
 
-		public double getValue() {
-			return sp;
-		}
+    public double getValue() {
+      return sp;
+    }
   }
 
-  // public enum ServoSP { //Servo Setpoints
-  //   ZERO(0), //NUMBERS NEED TO CHANGE
-  //   STOW(0), //NUMBERS NEED TO CHANGE 
-  //   DEPLOY(1); //NUMBERS NEED TO CHANGE
+  public enum HookSP { // Climber Setpoints
+    STOW(1000), // NUMBERS NEED TO CHANGE
+    DEPLOY(1250); // NUMBERS NEED TO CHANGE
 
-  //   private final double sp;
+    private final int sp;
 
-	// 	ServoSP(final double sp) {
-	// 		this.sp = sp;
-	// 	}
+    HookSP(final int sp) {
+      this.sp = sp;
+    }
 
-	// 	public double getValue() {
-	// 		return sp;
-	// 	}
-  // }
+    public int getPos() {
+      return sp;
+    }
+  }
+
+  private final ShuffleboardTab climberTab = Shuffleboard.getTab("Climber");
+
+  private final GenericEntry sbClimberOnTgt = climberTab.addPersistent("Climber OnTgt", false)
+      .withWidget("Boolean Box").withPosition(0, 1).withSize(2, 1).getEntry();
+  private final GenericEntry sbClimberSP = climberTab.addPersistent("Climber SP", "")
+      .withWidget("Text View").withPosition(2, 0).withSize(2, 1).getEntry();
+  private final GenericEntry sbClimberSPPos = climberTab.addPersistent("Climber SP Pos", 0)
+      .withWidget("Text View").withPosition(2, 1).withSize(2, 1).getEntry();
+
+  private final GenericEntry sbHookOnTgt = climberTab.addPersistent("Hook OnTgt", false)
+      .withWidget("Boolean Box").withPosition(0, 1).withSize(2, 1).getEntry();
+  private final GenericEntry sbHookSP = climberTab.addPersistent("Hook SP", "")
+      .withWidget("Text View").withPosition(2, 0).withSize(2, 1).getEntry();
+  private final GenericEntry sbHookSPPos = climberTab.addPersistent("Hook SP Pos", 0)
+      .withWidget("Text View").withPosition(2, 1).withSize(2, 1).getEntry();
 
   private ClimberSP climberSP = Climber.ClimberSP.STOW;
-	private Library lib = new Library();
+  private HookSP hookSP = Climber.HookSP.STOW;
 
-
-  //CONSTRUCTOR
+  // CONSTRUCTOR
   public Climber() {
     System.out.println("+++++ Starting Climber Constructor +++++");
 
     // Climbing Motor Configs 1-4
     climber1Config
-    .inverted(Constants.Climber.kClimberInverted)
-    .idleMode(Constants.Climber.kClimberIdleMode)
-    .smartCurrentLimit(Constants.Climber.kClimberCurrentLimit);
+        .inverted(Constants.Climber.kClimberInverted)
+        .idleMode(Constants.Climber.kClimberIdleMode)
+        .smartCurrentLimit(Constants.Climber.kClimberCurrentLimit);
     climber1Config.absoluteEncoder
-    .zeroOffset(Constants.Climber.kZeroOffset)
-    .zeroCentered(Constants.Climber.kZeroCentered)
-    .inverted(Constants.Climber.kEncoderInverted)
-    .positionConversionFactor(Constants.Climber.kTiltPositionFactor)
-    .velocityConversionFactor(Constants.Climber.kTiltVelocityFactor);
+        .zeroOffset(Constants.Climber.kZeroOffset)
+        .zeroCentered(Constants.Climber.kZeroCentered)
+        .inverted(Constants.Climber.kEncoderInverted)
+        .positionConversionFactor(Constants.Climber.kTiltPositionFactor)
+        .velocityConversionFactor(Constants.Climber.kTiltVelocityFactor);
     climber1Config.closedLoop
-    .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-    .p(Constants.Climber.kPosP)
-    .i(Constants.Climber.kPosI)
-    .d(Constants.Climber.kPosD)
-    .outputRange(Constants.Climber.kPosMinOutput, Constants.Climber.kPosMaxOutput);
-    
-    climber2Config.follow(Constants.CANId.kClimber1CanId); //Mimics climber1
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        .p(Constants.Climber.kPosP)
+        .i(Constants.Climber.kPosI)
+        .d(Constants.Climber.kPosD)
+        .outputRange(Constants.Climber.kPosMinOutput, Constants.Climber.kPosMaxOutput);
+
+    climber2Config.follow(Constants.CANId.kClimber1CanId); // Mimics climber1
     climber3Config.follow(Constants.CANId.kClimber1CanId);
     climber4Config.follow(Constants.CANId.kClimber1CanId);
 
-    climber1.configure(climber1Config, 
-      com.revrobotics.ResetMode.kNoResetSafeParameters,
-      com.revrobotics.PersistMode.kPersistParameters);
+    climber1.configure(climber1Config,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
 
-    climber2.configure(climber2Config, 
-      com.revrobotics.ResetMode.kNoResetSafeParameters,
-      com.revrobotics.PersistMode.kPersistParameters);
-    
-    climber3.configure(climber3Config, 
-      com.revrobotics.ResetMode.kNoResetSafeParameters,
-      com.revrobotics.PersistMode.kPersistParameters);
+    climber2.configure(climber2Config,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
 
-    climber4.configure(climber4Config, 
-      com.revrobotics.ResetMode.kNoResetSafeParameters,
-      com.revrobotics.PersistMode.kPersistParameters);
+    climber3.configure(climber3Config,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
 
-    // servoHubConfig
-    // .channel0.pulseRange(500, 1500, 2500)
-    // .disableBehavior(ServoChannelConfig.BehaviorWhenDisabled.kSupplyPower); //Default is 0-180, but can be changed if needed
-    // servoHub.configure(
-    //   servoHubConfig, 
-    //   com.revrobotics.ResetMode.kResetSafeParameters);
+    climber4.configure(climber4Config,
+        com.revrobotics.ResetMode.kNoResetSafeParameters,
+        com.revrobotics.PersistMode.kPersistParameters);
 
-    // Set the pulse period for channels 0-2 to 5ms (5000 microseconds)
-    //servoHub.setBankPulsePeriod(ServoHub.Bank.kBank0_2, 5000);
-    
+    hubConfig.channel0.pulseRange(500, 1500, 2500)
+        .disableBehavior(ServoChannelConfig.BehaviorWhenDisabled.kSupplyPower); // Default is 0-180, but can be changed
+                                                                                // if
+    hubConfig.channel1.pulseRange(500, 1500, 2500)
+        .disableBehavior(ServoChannelConfig.BehaviorWhenDisabled.kSupplyPower); // Default is 0-180, but can be changed
+                                                                                // if
+    // needed
+    servoHub.configure(hubConfig,
+        com.revrobotics.ResetMode.kResetSafeParameters);
+
+    leftHook.setPowered(true);
+    rightHook.setPowered(true);
+    leftHook.setEnabled(true);
+    rightHook.setEnabled(true);
 
     System.out.println("+++++ End of Climber Constructor +++++");
-    }
+  }
 
+  // COMMANDS
+  public Command setHooks( HookSP sp) {
+    return runOnce(() -> setHookPos(sp));
+  }
 
- //COMMANDS
-  // public Command exampleMethodCommand() {
-  //   // Inline construction of command goes here.
-  //   // Subsystem::RunOnce implicitly requires `this` subsystem.
-  //   return runOnce(
-  //       () -> {
-  //         /* one-time action goes here */
-  //       });
-  // }
+  public Command setClimber( ClimberSP sp) {
+    return runOnce(() -> setClimberPos(sp));
+  }
 
-  //METHODS
-  //Start of Climber Methods
+  // PERIOIDC
+  @Override
+  public void periodic() {
+    sbClimberOnTgt.setBoolean(onClimberTarget());
+    sbClimberSP.setString(getClimberSP().name());
+    sbClimberSPPos.setDouble(getClimberSP().getValue());
+    sbHookOnTgt.setBoolean(onHookTarget());
+    sbHookSP.setString(getHookSP().name());
+    sbHookSPPos.setDouble(getHookSP().getPos());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  // METHODS
+  // Start of Climber Methods
   public double getClimberPos() {
-   return climber1AbsEncoder.getPosition(); //All the other motors should match
+    return climber1AbsEncoder.getPosition(); // All the other motors should match
   }
 
   public double getClimberVel() {
-    return climber1AbsEncoder.getVelocity(); //All the other motors should match
+    return climber1AbsEncoder.getVelocity(); // All the other motors should match
   }
-  
+
   public void setClimberPos(ClimberSP pos) {
     setClimberSP(pos);
-    climber1Controller.setSetpoint(pos.getValue(), 
-      SparkBase.ControlType.kPosition);
+    climber1Controller.setSetpoint(pos.getValue(),
+        SparkBase.ControlType.kPosition);
   }
 
   public void setClimberPos() {
-    setClimberPos(getClimberSP()); 
-    //Sets the desired position using the current setpoint,
+    setClimberPos(getClimberSP());
+    // Sets the desired position using the current setpoint,
     // which is updated by the other setClimberPos method.
   }
 
@@ -198,25 +237,30 @@ public class Climber extends SubsystemBase {
     return climberSP;
   }
 
-  public boolean onTarget() {
-		return Math.abs(getClimberPos() - getClimberSP().getValue()) < Constants.Climber.kTollerance ||
-				Math.abs(getClimberPos() - getClimberSP().getValue()) < Constants.Climber.kTollerance;
-	}
-  //End of Climber Methods
-  //Start of Servo Methods
+  public boolean onClimberTarget() {
+    return Math.abs(getClimberPos() - getClimberSP().getValue()) < Constants.Climber.kClimberTollerance;
+  }
+  // End of Climber Methods
+  // Start of Servo Methods
 
-  
-
-
-
-  //PERIOIDC
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void setHookSP(HookSP sp) {
+    hookSP = sp;
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public HookSP getHookSP() {
+    return hookSP;
+  }
+
+  public void setHookPos(HookSP pos) {
+    leftHook.setPulseWidth(pos.getPos());
+    rightHook.setPulseWidth(pos.getPos());
+  }
+
+  public double getHookPos() {
+    return ((leftHook.getPulseWidth() + rightHook.getPulseWidth()) / 2.0); // Average of the two hooks
+  }
+
+  public boolean onHookTarget() {
+    return Math.abs(getHookPos() - getHookSP().getPos()) < Constants.Climber.kHookTollerance;
   }
 }
